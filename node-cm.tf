f8e3353d590a99c9683f4cfe5a48d50b92ab32cd
Takeshi Kondo <takeshi.kondo@gmail.com>
2018-09-24 12:03:12 +0900

    First Commit

---
+++ node-cm.tf
@@ -0,0 +1,22 @@
+locals {
+  config-map-aws-auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.demo-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+}
+
+output "config-map-aws-auth" {
+  value = "${local.config-map-aws-auth}"
+}
